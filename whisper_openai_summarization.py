f71bd678d11a58101ed36c5b9ff39eb161a5faeb
Hong Tang <53389274+stanghong@users.noreply.github.com>
2023-10-14 16:45:58 -0700

    Create whisper_openai_summarization.py

---
+++ whisper_openai_summarization.py
@@ -0,0 +1,30 @@
+
+import openai
+import whisper
+# brew install ffmpeg # you may need to install
+# ref https://medium.com/gitconnected/using-the-whisper-api-to-transcribe-audio-files-45fb36d1aa1b
+
+
+openai.api_key = 'YOUROPENAPIKEY'
+model = whisper.load_model("base")
+
+file_path = 'MA1.m4a'
+
+def transcribe_audio(model, file_path):
+    transcript = model.transcribe(file_path)
+    return transcript['text']
+
+def CustomChatGPT(user_input):
+    messages = [{"role": "system", "content": "You are an office administer, summarize the text in key points"}]
+    messages.append({"role": "user", "content": user_input})
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = messages
+    )
+    ChatGPT_reply = response["choices"][0]["message"]["content"]
+    return ChatGPT_reply
+
+
+transcription = transcribe_audio(model, file_path)
+summary = CustomChatGPT(transcription)
+print(summary)
